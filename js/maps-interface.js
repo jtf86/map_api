7897fb8d3cf41d1a68cddf23ab7558592f4ee6af
John <john@epicodus.com>
2016-08-17 16:52:57 -0700

    map works, focused on greenwich

--- js/maps-interface.js
+++ js/maps-interface.js
@@ -7,8 +7,8 @@ var apiKey = require('./../.env').apiKey;
 function initMap() {
   var mapDiv = document.getElementById('map');
   var map = new google.maps.Map(mapDiv, {
-    center: {lat: 44.540, lng: -78.546},
-    zoom: 8
+    center: {lat: 51.477, lng: 0},
+    zoom: 19
   });
 }
 
